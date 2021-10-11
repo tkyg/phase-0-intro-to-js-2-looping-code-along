7682d47ae57f4afff6822a0730f31bb54ce1a4b3
Tenzin Tenkyong <ttenkyong@gmail.com>
2021-10-11 12:26:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let messages = [];
+    for(let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+
+    
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0){
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
