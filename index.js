156fb99173947ea705accbfbcf293480a6726b3d
tkyg <80114433+tkyg@users.noreply.github.com>
2021-11-11 19:49:12 -0500

    commit tree 56ebb630b60b17d81089ac37871dca1fa34cc2e3

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-function writeCards(name, event) {
-    let messages = [];
-    for(let i = 0; i < name.length; i++) {
-        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
-    }
-    return messages;
-}
+// function writeCards(name, event) {
+//     let messages = [];
+//     for(let i = 0; i < name.length; i++) {
+//         messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+//     }
+//     return messages;
+// }
 
     
-function countDown(positiveNumber) {
-    while(positiveNumber >= 0){
-        console.log(positiveNumber);
-        positiveNumber--;
+// function countDown(positiveNumber) {
+//     while(positiveNumber >= 0){
+//         console.log(positiveNumber);
+//         positiveNumber--;
+//     }
+// }
+
+function writeCards(name, event){
+    let messages = []
+    for (let i = 0; i < name.length; i++){
+     messages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
     }
-}
+    return messages;
+  }
+
+  function countDown(positiveNumber){
+      let i = 0
+      while(i <= 10){
+          console.log(i++)
+      }
+  }
